a92f4a0af0ce8e4514002954afeb443b602df758
Alex Zorin <alex@zorin.id.au>
2018-04-29 22:51:19 +1000

    dns: make DNSSEC error message clearer for users

--- dns_util.go
+++ dns_util.go
@@ -27,7 +27,7 @@ func lookup(name string, rrType uint16) ([]dns.RR, error) {
 	}
 
 	if result.Bogus {
-		return nil, fmt.Errorf("Response for %s was bogus: %v", name, result.WhyBogus)
+		return nil, fmt.Errorf("DNS response for %s had fatal DNSSEC issues: %v", name, result.WhyBogus)
 	}
 
 	return result.Rr, nil
