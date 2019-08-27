letsdebug/letsdebug

commit e7be8dae01ec98433c95cb3f3f9082f9b710cd30

multipleIPAddress: filter out dead servers
This prevents us getting unhelpful multipleIPAddressDiscrepancy
problems responses that compare e.g. HTTP status 200 to 0

http01.go
@@ -132,9 +132,17 @@ func (c httpAccessibilityChecker) Check(ctx *scanContext, domain string, method 
 			domain, ip.String(), res.String(), prob.Name, strings.Join(res.DialStack, "\n")))
 	}
 
-	firstResult := allCheckResults[0]
-	if len(allCheckResults) > 1 {
-		for _, otherResult := range allCheckResults[1:] {
+	// Filter out the servers that didn't respond at all
+	var nonZeroResults []httpCheckResult
+	for _, v := range allCheckResults {
+		if v.IsZero() {
+			continue
+		}
+		nonZeroResults = append(nonZeroResults, v)
+	}
+	firstResult := nonZeroResults[0]
+	if len(nonZeroResults) > 1 {
+		for _, otherResult := range nonZeroResults[1:] {
 			if firstResult.StatusCode != otherResult.StatusCode ||
 				firstResult.ServerHeader != otherResult.ServerHeader ||
 				firstResult.NumRedirects != otherResult.NumRedirects ||

commit a35d61fdc5ccdec2a0d692b942b500e3a7397a7f

fix panic in httpAccessibilityChecker
Was probably introduced in e7be8dae01ec98433c95cb3f3f9082f9b710cd30

http01.go
@@ -140,8 +140,8 @@ func (c httpAccessibilityChecker) Check(ctx *scanContext, domain string, method 
 		}
 		nonZeroResults = append(nonZeroResults, v)
 	}
-	firstResult := nonZeroResults[0]
 	if len(nonZeroResults) > 1 {
+		firstResult := nonZeroResults[0]
 		for _, otherResult := range nonZeroResults[1:] {
 			if firstResult.StatusCode != otherResult.StatusCode ||
 				firstResult.ServerHeader != otherResult.ServerHeader ||